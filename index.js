f36b9243fa8bb1cd98db97549fc36a18766d0c3f
wangjinbo <vannue@qq.com>
2019-01-04 23:54:12 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-$(".menuitem>a").forEach(item=>{
-  let $item = item;
-  $item.on('click',(e)=>{
-    console.log(e.currentTarget)
+var menus = $(".menuitem>a")
+let current = 0;
+active(getItem(0));
+$.each(menus,key=>{
+  $(menus[key]).on('click',(e)=>{
+    e.preventDefault();
+    jump(key);
+    active(getItem(key));
+    current = key;
   })
 })
+
+setInterval(()=>{
+  current++;
+  jump(fix(current));
+  active(getItem(fix(current)))
+},10000)
+
+function jump(index){
+  $('.images').css('transform',()=>{return `translateX(${-920*index}px)`})
+}
+function fix(n){
+  return n =  n % menus.length 
+}
+function getItem(n){
+  return $(`.menu>ul>li:nth-child(${n+1})`);
+}
+function active($node){
+  $(".menu>ul>li.active").removeClass("active");
+  $node.addClass("active");
+}
